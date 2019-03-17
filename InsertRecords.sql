c5c6255898efd61b515dde082ea68e8da1f265a5
Rohit Gollarahalli <rohit.gs28@gmail.com>
2019-03-17 15:17:14 -0400

    Create InsertRecords.sql

---
+++ InsertRecords.sql
@@ -0,0 +1,22 @@
+insert into user(id,`PatientMRN`,  `firstname` , `lastname`,`DOB`,`Address`,`Phonenumber`)
+VALUES(1,'12345676','Rohit','Gollarahalli','28-08-1992','66A Pembroke street','902-989-4524')
+
+
+insert into user(id,`PatientMRN`,  `firstname` , `lastname`,`DOB`,`Address`,`Phonenumber`)
+VALUES(2,'23456765','Prashant','TK','17-08-1993','100 Cadorna avenue','902-989-3456')
+
+insert into user(id,`PatientMRN`,  `firstname` , `lastname`,`DOB`,`Address`,`Phonenumber`)
+VALUES(3,'23456768','Mike','Ross','29-09-1993','East york','902-989-3456')
+
+
+insert into user(id,`PatientMRN`,  `firstname` , `lastname`,`DOB`,`Address`,`Phonenumber`)
+VALUES(4,'23456769','Harvey','Spectar','29-09-1993','East york','902-989-3456')
+
+insert into user(id,`PatientMRN`,  `firstname` , `lastname`,`DOB`,`Address`,`Phonenumber`)
+VALUES(5,'23456710','Jessica','Pearson','29-09-1997','East york','902-989-3456')
+
+insert into user(id,`PatientMRN`,  `firstname` , `lastname`,`DOB`,`Address`,`Phonenumber`)
+VALUES(6,'23456711','Daniel','Hardman','1-23-1980','Cosburn Street','902-989-3456')
+
+insert into user(id,`PatientMRN`,  `firstname` , `lastname`,`DOB`,`Address`,`Phonenumber`)
+VALUES(7,'23456712','Racheal','Whiss','2-07-1978','south park street','902-989-3456')
